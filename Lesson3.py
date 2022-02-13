8a86ba4a36b43c6bd655de0a45787ec649cf28c5
R3bbit01 <langston.greggory@Gmail.com>
2022-02-13 14:31:06 -0800

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -1,5 +1,5 @@
 num1 = float(input("What is the price of a Child's meal? " ))
-num2 = float(input("What is the price of an adult's mean? "))
+num2 = float(input("What is the price of an adult's meal? "))
 num3 = int(input("How many children are there? "))
 num4 = int(input("How many adults are there? " ))
 num5 = float(input("What is the sales tax rate? "))
